c3239d03c588e1359c18e99a5fcd6059614da50d
scottwing <scottnwing@gmail.com>
2016-11-08 15:45:35 -0700

    Create IdKeywordTest.c

---
+++ IdKeywordTest.c
@@ -0,0 +1,28 @@
+/***************************************************************************//**
+* \file    hal_uart.c
+*
+* \author  A. Sowada
+* \date    12/14/2015
+*
+* \brief   I2C hardware abstraction implementation.
+* $Date: $
+* $Revision: $
+* $Author: $
+* $HeadURL: $
+* $Id: $
+*
+* $Id$
+* $Date$
+* $File$
+* $Author$
+* $Revision$
+* $Source$
+*******************************************************************************/
+#include <driverlib/i2c.h>
+#include <driverlib/prcm.h>
+#include <utlTypes.h>
+#include <driverlib/ioc.h>
+
+#include "hal_gpio.h"
+#include "hal_i2c.h"
+
